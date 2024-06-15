5638891e0e4f140052f2b9a7f0b260dc49e0ddcf
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-06-15 22:24:05 +1000

    chord

--- src/main/java/garden/hestia/powerchord/InstrumentItem.java
+++ src/main/java/garden/hestia/powerchord/InstrumentItem.java
@@ -1,5 +1,7 @@
 package garden.hestia.powerchord;
 
+import net.minecraft.block.NoteBlock;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -16,12 +18,41 @@ public class InstrumentItem extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        user.playSound(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO.value(), 1.0f, 4.0f);
+        user.setCurrentHand(hand);
+        user.playSound(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO.value(), 1.0f, NoteBlock.getNotePitch(0));
         return TypedActionResult.pass(user.getStackInHand(hand));
+    }
+
+    @Override
+    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        if (remainingUseTicks == 50) {
+            user.playSound(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO.value(), 1.0f, NoteBlock.getNotePitch(4));
+        } else if (remainingUseTicks == 40) {
+            user.playSound(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO.value(), 1.0f, NoteBlock.getNotePitch(7));
+        }
+        super.usageTick(world, user, stack, remainingUseTicks);
+    }
+
+    @Override
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        if (remainingUseTicks < 40) {
+            user.playSound(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO.value(), 0.5f, NoteBlock.getNotePitch(0));
+            user.playSound(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO.value(), 0.3f, NoteBlock.getNotePitch(4));
+            user.playSound(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO.value(), 0.7f, NoteBlock.getNotePitch(7));
+            if (user instanceof PlayerEntity player) {
+                player.getItemCooldownManager().set(stack.getItem(), 60);
+            }
+        }
+        super.onStoppedUsing(stack, world, user, remainingUseTicks);
     }
 
     @Override
     public UseAction getUseAction(ItemStack stack) {
         return UseAction.BOW;
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack, LivingEntity user) {
+        return 60;
     }
 }
