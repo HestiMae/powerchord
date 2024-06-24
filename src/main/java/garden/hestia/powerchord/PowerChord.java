67ae3e39f3b82005fe8a999df87745856fcc4266
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-06-24 17:56:47 +1000

    add remaining valley spirits

--- src/main/java/garden/hestia/powerchord/PowerChord.java
+++ src/main/java/garden/hestia/powerchord/PowerChord.java
@@ -5,8 +5,11 @@ import garden.hestia.powerchord.component.InstrumentStateComponent;
 import garden.hestia.powerchord.component.KeyComponent;
 import garden.hestia.powerchord.music.Progressions;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.component.ComponentType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvents;
@@ -30,13 +33,13 @@ public class PowerChord implements ModInitializer {
     public static final ComponentType<InstrumentStateComponent> STATE = Registry.register(Registries.DATA_COMPONENT_TYPE, Identifier.of(ID, "state"),
         ComponentType.<InstrumentStateComponent>builder().packetCodec(InstrumentStateComponent.PACKET_CODEC).build());
 
-    public static final InstrumentItem PAN_FLUTE = Registry.register(Registries.ITEM, Identifier.of(ID, "pan_flute"), new InstrumentItem(new Item.Settings().maxCount(1).rarity(Rarity.UNCOMMON)
+    public static final InstrumentItem PAN_FLUTE = Registry.register(Registries.ITEM, Identifier.of(ID, "pan_flute"), new InstrumentItem(new Item.Settings().maxCount(1).rarity(Rarity.COMMON)
         .component(KEY, new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_FLUTE.value(), Progressions.CHROMATIC))
         .component(INSTRUMENT, new InstrumentComponent(10, 20))
     ));
 
-    public static final InstrumentItem HARMONICA = Registry.register(Registries.ITEM, Identifier.of(ID, "harmonica"), new InstrumentItem(new Item.Settings().maxCount(1).rarity(Rarity.UNCOMMON).component(KEY,
-        new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO.value(), Progressions.CHROMATIC_MINOR))
+    public static final InstrumentItem HARMONICA = Registry.register(Registries.ITEM, Identifier.of(ID, "harmonica"), new InstrumentItem(new Item.Settings().maxCount(1).rarity(Rarity.COMMON).component(KEY,
+            new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO.value(), Progressions.CHROMATIC_MINOR))
         .component(INSTRUMENT, new InstrumentComponent(5, 40))
     ));
 
@@ -60,12 +63,67 @@ public class PowerChord implements ModInitializer {
         new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_BASS.value(), Progressions.D1546)))
     );
 
+    public static final Item SPIRIT_ECHIDNA = Registry.register(Registries.ITEM, Identifier.of(ID, "spirit_echidna"), new Item(new Item.Settings().maxCount(1).rarity(Rarity.RARE).component(KEY,
+        new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_XYLOPHONE.value(), Progressions.E1546)))
+    );
+
+    public static final Item SPIRIT_FROG = Registry.register(Registries.ITEM, Identifier.of(ID, "spirit_frog"), new Item(new Item.Settings().maxCount(1).rarity(Rarity.RARE).component(KEY,
+        new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_IRON_XYLOPHONE.value(), Progressions.F1546)))
+    );
+
+    public static final Item SPIRIT_GOPHER = Registry.register(Registries.ITEM, Identifier.of(ID, "spirit_gopher"), new Item(new Item.Settings().maxCount(1).rarity(Rarity.RARE).component(KEY,
+        new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_COW_BELL.value(), Progressions.G1546)))
+    );
+
+    public static final Item SPIRIT_ANTEATER = Registry.register(Registries.ITEM, Identifier.of(ID, "spirit_anteater"), new Item(new Item.Settings().maxCount(1).rarity(Rarity.RARE).component(KEY,
+        new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_DIDGERIDOO.value(), Progressions.Am1546)))
+    );
+
+    public static final Item SPIRIT_BUTTERFLY = Registry.register(Registries.ITEM, Identifier.of(ID, "spirit_butterfly"), new Item(new Item.Settings().maxCount(1).rarity(Rarity.RARE).component(KEY,
+        new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_HARP.value(), Progressions.Bm1546)))
+    );
+
     public static final Item SPIRIT_CHAMELEON = Registry.register(Registries.ITEM, Identifier.of(ID, "spirit_chameleon"), new Item(new Item.Settings().maxCount(1).rarity(Rarity.RARE).component(KEY,
         new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_GUITAR.value(), Progressions.Cm1546)))
     );
 
+    public static final Item SPIRIT_DEER = Registry.register(Registries.ITEM, Identifier.of(ID, "spirit_deer"), new Item(new Item.Settings().maxCount(1).rarity(Rarity.RARE).component(KEY,
+        new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_FLUTE.value(), Progressions.Dm1546)))
+    );
+
+    public static final Item SPIRIT_EMU = Registry.register(Registries.ITEM, Identifier.of(ID, "spirit_emu"), new Item(new Item.Settings().maxCount(1).rarity(Rarity.RARE).component(KEY,
+        new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_BANJO.value(), Progressions.Em1546)))
+    );
+
+    public static final Item SPIRIT_FLAMINGO = Registry.register(Registries.ITEM, Identifier.of(ID, "spirit_flamingo"), new Item(new Item.Settings().maxCount(1).rarity(Rarity.RARE).component(KEY,
+        new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_PLING.value(), Progressions.Fm1546)))
+    );
+
+    public static final Item SPIRIT_GRASSHOPPER = Registry.register(Registries.ITEM, Identifier.of(ID, "spirit_grasshopper"), new Item(new Item.Settings().maxCount(1).rarity(Rarity.RARE).component(KEY,
+        new KeyComponent(SoundEvents.BLOCK_NOTE_BLOCK_BIT.value(), Progressions.Gm1546)))
+    );
+
     @Override
     public void onInitialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register((entries -> entries.addBefore(Items.MUSIC_DISC_13,
+            PAN_FLUTE,
+            HARMONICA,
+            SPIRIT_ARMADILLO,
+            SPIRIT_ANTEATER,
+            SPIRIT_BUNNY,
+            SPIRIT_BAT,
+            SPIRIT_BUTTERFLY,
+            SPIRIT_CAT,
+            SPIRIT_CHAMELEON,
+            SPIRIT_DOG,
+            SPIRIT_DEER,
+            SPIRIT_ECHIDNA,
+            SPIRIT_EMU,
+            SPIRIT_FROG,
+            SPIRIT_FLAMINGO,
+            SPIRIT_GOPHER,
+            SPIRIT_GRASSHOPPER
+        )));
         LOGGER.info("[PowerChord] 1, 2, 3 - 123!");
     }
 }
--- src/main/java/garden/hestia/powerchord/music/Progressions.java
+++ src/main/java/garden/hestia/powerchord/music/Progressions.java
@@ -13,6 +13,15 @@ public class Progressions {
     public static final List<PowerableChord> Bb1547 = List.of(Chords.Bb, Chords.Gm, Chords.F, Chords.Adim);
     public static final List<PowerableChord> C1546 = List.of(Chords.C, Chords.G, Chords.F, Chords.Am);
     public static final List<PowerableChord> D1546 = List.of(Chords.D, Chords.A, Chords.G, Chords.Bm);
+    public static final List<PowerableChord> E1546 = List.of(Chords.E, Chords.B, Chords.A, Chords.Csm);
+    public static final List<PowerableChord> F1546 = List.of(Chords.F, Chords.C, Chords.Bb, Chords.Dm);
+    public static final List<PowerableChord> G1546 = List.of(Chords.G, Chords.D, Chords.C, Chords.Em);
 
+    public static final List<PowerableChord> Am1546 = List.of(Chords.Am, Chords.Em, Chords.Dm, Chords.F);
+    public static final List<PowerableChord> Bm1546 = List.of(Chords.Bm, Chords.Fsm, Chords.Em, Chords.G);
     public static final List<PowerableChord> Cm1546 = List.of(Chords.Cm, Chords.Gm, Chords.Fm, Chords.Ab);
+    public static final List<PowerableChord> Dm1546 = List.of(Chords.Dm, Chords.Am, Chords.Gm, Chords.Bb);
+    public static final List<PowerableChord> Em1546 = List.of(Chords.Em, Chords.Bm, Chords.Am, Chords.C);
+    public static final List<PowerableChord> Fm1546 = List.of(Chords.Fm, Chords.Cm, Chords.Bbm, Chords.Db);
+    public static final List<PowerableChord> Gm1546 = List.of(Chords.Gm, Chords.Dm, Chords.Cm, Chords.Eb);
 }
