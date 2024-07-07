6b59d345ad513ce73561fef2f1158ca6e0c6fb10
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-07-08 00:23:26 +1000

    actually send the packet

--- src/main/java/garden/hestia/powerchord/mixin/client/MixinMinecraftClient.java
+++ src/main/java/garden/hestia/powerchord/mixin/client/MixinMinecraftClient.java
@@ -1,6 +1,8 @@
 package garden.hestia.powerchord.mixin.client;
 
+import garden.hestia.powerchord.FreePlayChordPacket;
 import garden.hestia.powerchord.InstrumentItem;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,6 +15,7 @@ public class MixinMinecraftClient {
     @Inject(method = "doAttack", at = @At("HEAD"), cancellable = true)
     public void freePlayChord(CallbackInfoReturnable<Boolean> cir) {
         if (MinecraftClient.getInstance().player != null && InstrumentItem.swing(MinecraftClient.getInstance().player, Hand.MAIN_HAND)) {
+            ClientPlayNetworking.send(new FreePlayChordPacket());
             cir.setReturnValue(false);
             cir.cancel();
         }
